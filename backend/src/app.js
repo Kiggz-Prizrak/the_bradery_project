ede51d057dd295e2d4c9c73538b8faf1081860c0
Kiggz-Prizrak <nico.peignot@outlook.fr>
2023-11-05 23:12:54 +0100

    feat(app): config route in app with express

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,4 +2,26 @@ const express = require("express");
 
 const app = express();
 
+const usersRoutes = require("./routes/users");
+const productsRoutes = require("./routes/products");
+const commandsRoutes = require("./routes/commands");
+
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use('/api/users', usersRoutes);
+app.use('/api/products', productsRoutes);
+app.use('/api/commands', commandsRoutes);
+
 module.exports = app;
