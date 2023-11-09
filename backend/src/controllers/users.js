f65497fad8224c1e3998a0eaa163e87cd0742c70
Kiggz-Prizrak <nico.peignot@outlook.fr>
2023-11-09 15:29:58 +0100

    feat(controllers & services) : init users database services and migration from controllers

--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -1,18 +1,14 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { promises: fs } = require("fs");
 const { models } = require("../models");
+const userServices = require("../services/users");
 
 exports.signup = async (req, res) => {
   const userObject = req.body;
 
-  const userEmailFind = await models.User.findOne({
-    where: { email: req.body.email },
-  });
+  if (await userServices.mailChecker(req.body.email))
+    return res.status(400).json({ message: "email  already used" });
 
-  if (userEmailFind) {
-    return res.status(400).json({ message: "email or username already used" });
-  }
   if (
     typeof userObject.email !== "string" ||
     typeof userObject.password !== "string" ||
@@ -53,16 +49,12 @@ exports.signup = async (req, res) => {
 
   delete req.body.isAdmin;
 
-  const user = models.User.create({
-    isAdmin: false,
-    email: req.body.email,
-    password: hash,
-    username: req.body.username,
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-  });
-  if (user) return res.status(201).json({ message: "Utilisateur créé" });
+  if (
+    userServices.createNewUser({ ...req.body, password: hash, isAdmin: false })
+  )
+    return res.status(201).json({ message: "user created" });
 };
+
 exports.login = async (req, res) => {
   if (
     typeof req.body.email !== "string" ||
@@ -71,11 +63,7 @@ exports.login = async (req, res) => {
     return res.status(400).json({ message: "please provides valid data" });
   }
 
-  const user = await models.User.findOne({
-    where: { email: req.body.email },
-  }).catch(async (error) => {
-    res.status(500).json({ message: "An error has occurred", error });
-  });
+  const user = await userServices.userFinder(req.body.email);
 
   if (!user) {
     return res.status(404).json({ message: "no user match with this mail" });
@@ -105,37 +93,21 @@ exports.getAllUsers = async (req, res) => {
   if (!req.auth.isAdmin) {
     return res.status(401).json({ message: "Unauthorized request" });
   }
-  const users = await models.User.findAll({
-    order: [["createdAt", "DESC"]],
-  }).catch((error) => res.status(400).json({ message: "bad request", error }));
+  const users = await userServices.usersGetter();
   return res.status(200).json(users);
 };
 
 exports.getOneUser = async (req, res) => {
-  const user = await models.User.findOne({
-    where: { id: req.params.id },
-  }).catch((error) =>
-    res.status(404).json({ message: "user not found", error })
-  );
+  const user = await userServices.userGetterOne(req.params.id);
   return res.status(200).json(user);
 };
 
 exports.modifyUser = async (req, res) => {
-  const userModifier = await models.User.findOne({
-    where: { id: req.params.id },
-  });
-  if (userModifier === null) {
-    return res.status(404).json({ message: "User not found" });
-  }
+  const userModifier = await userServices.userGetterOne(req.params.id);
 
   if (req.body.email) {
-    const userEmailFind = await models.User.findOne({
-      where: { email: req.body.email },
-    });
-    if (userEmailFind) {
-      return res
-        .status(400)
-        .json({ message: "email or username already used" });
+    if (await userServices.mailChecker(req.body.email)) {
+      return res.status(400).json({ message: "email  already used" });
     }
   }
 
@@ -170,27 +142,16 @@ exports.modifyUser = async (req, res) => {
     }
   }
 
-  await models.User.update(
-    { ...req.body, id: req.params.id },
-    { where: { id: req.params.id } }
-  ).catch((error) => res.status(400).json({ error }));
-
-  const user = await models.User.findOne({
-    where: { id: req.params.id },
-  });
+  await userServices.updateUser({ ...req.body, id: req.params.id });
+  const user = await userServices.userGetterOne(req.params.id);
   return res.status(200).json({ message: "User modifié", user });
 };
 
 exports.deleteUser = async (req, res) => {
-  const user = await models.User.findOne({ where: { id: req.params.id } });
-  if (user === null) {
-    return res.status(404).json({ message: "User not found" });
-  }
+  const user = await userServices.userGetterOne(req.params.id);
   if (user.id !== req.auth.UserId && !req.auth.isAdmin) {
     return res.status(401).json({ message: "Unauthorized request" });
   }
-  await models.User.destroy({ where: { id: req.params.id } }).catch((error) =>
-    res.status(400).json({ error })
-  );
+  await userServices.deleteUser(req.params.id);
   return res.status(200).json({ message: "Objet supprimé !" });
 };
---
+++ backend/src/services/users.js
@@ -0,0 +1,47 @@
+const { models } = require("../models");
+
+exports.mailChecker = (email) => {
+  return models.User.findOne({
+    where: { email },
+  });
+};
+
+exports.createNewUser = (data) => {
+  return models.User.create({
+    ...data,
+  });
+};
+
+exports.userFinder = (email) => {
+  return models.User.findOne({
+    where: { email },
+  }).catch(async (error) => {
+    res.status(500).json({ message: "An error has occurred", error });
+  });
+};
+
+exports.usersGetter = () => {
+  return models.User.findAll({
+    order: [["createdAt", "DESC"]],
+  }).catch((error) => res.status(400).json({ message: "bad request", error }));
+};
+
+exports.userGetterOne = (id) => {
+  return models.User.findOne({
+    where: { id },
+  }).catch((error) =>
+    res.status(404).json({ message: "user not found", error })
+  );
+};
+
+exports.updateUser = (data) => {
+  return models.User.update(data, { where: { id: data.id } }).catch(
+    (error) => res.status(400).json({ error })
+  );
+};
+
+exports.deleteUser = (id) => {
+  return models.User.destroy({ where: { id } }).catch((error) =>
+    res.status(400).json({ error })
+  );
+}
