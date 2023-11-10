88eda76d998494f6615f8b9995db568d49ecdb94
Kiggz-Prizrak <nico.peignot@outlook.fr>
2023-11-10 11:38:03 +0100

    feat(router): config router

---
+++ frontend/src/router/index.jsx
@@ -0,0 +1,54 @@
+import { createBrowserRouter } from "react-router-dom";
+
+// import {
+//   getAllProducts
+//   getOneProduct,
+//   getUserData
+// } from "../api";
+
+import Root from "./Root";
+
+import Login from "../pages/Login";
+import Product from "../pages/Article";
+import Account from "../pages/Account";
+import ErrorPage from "../pages/ErrorPage";
+import Home from "../pages/Home";
+import PaySelect from "../pages/PaySelect";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        // loader: () => {
+        //   return getAllArticles();
+        // },
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+        // loader: ({ params }) => {
+        //   return getOneProduct(params.id);
+        // },
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/account",
+        element: <Account />,
+      },
+      {
+        path: "/PaySelect",
+        element: <PaySelect />,
+      },
+    ],
+  },
+]);
+
+export default router;
