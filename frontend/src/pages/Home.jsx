9def37cb3e83dc771219252ff13edb4127a4605b
Kiggz-Prizrak <nico.peignot@outlook.fr>
2023-11-13 03:26:59 +0100

    feat: home product search engine

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,39 @@
 import { useLoaderData } from "react-router-dom";
 import ProductCard from "../components/ProductCard";
-
-ProductCard;
+import { useEffect, useState } from "react";
 
 const Home = () => {
-  const productList = useLoaderData();
+  const data = useLoaderData();
 
-  console.log(productList);
+  const [productList, setProductList] = useState(Array.from(data));
+  const [resultSearchProduct, setResultSearchProduct] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
+  useEffect(() => {
+    if (searchValue) {
+      const listMatch = productList.filter((list) =>
+        list.name.toLowerCase().includes(searchValue.toLowerCase())
+      );
+      setResultSearchProduct(listMatch);
+    } else {
+      console.log(searchValue)
+      setResultSearchProduct(Array.from(data));
+    }
+  }, [searchValue]);
+
 
   return (
-    <main>
-      <div>
+    <main className="home_container">
+      
+        <h2> search product</h2>
+        <div className="product_search">
+          <input type="text" onChange={(e) => setSearchValue(e.target.value)} />
+          <span>
+            <i className="fa-solid fa-magnifying-glass"></i>
+          </span>
+        </div>
         <div className="productList_container">
-          {productList.map((product) => (
+          {resultSearchProduct.map((product) => (
             <ProductCard
               key={product.id}
               name={product.name}
@@ -21,7 +42,7 @@ const Home = () => {
             />
           ))}
         </div>
-      </div>
+     
     </main>
   );
 };
